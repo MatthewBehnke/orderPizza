31e03aaa6c74bea62ed296771f49aaeb74c3f8bd
matthewbehnke <matthewbehnke0@gmail.com>
2019-04-02 14:43:54 -0500

    added pages

--- app/src/main/java/com/example/orderpizza/MainActivity.java
+++ app/src/main/java/com/example/orderpizza/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.orderpizza;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addPerson;
+    Button restart;
+    Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addPerson = findViewById(R.id.createPerson);
+        restart = findViewById(R.id.reset);
+        calculate = findViewById(R.id.calculate);
     }
+
+
+    public void setAddPerson(View v) {
+        Intent intent = new Intent(this, AddPerson.class);
+        startActivity(intent);
+    }
+
+    public void setRestart(View v) {
+
+    }
+
+    public void setCalculate(View v) {
+        Intent intent = new Intent(this, Caculate.class);
+        startActivity(intent);
+    }
+
 }
